902f5e8fa06050c4686e78c2f555f67180db9fc2
Jiaxiang Zheng <jiaxiang.zheng135@gmail.com>
2015-01-09 09:10:42 +0800

    update

--- learnyounode/http-file-server.js
+++ learnyounode/http-file-server.js
@@ -5,6 +5,7 @@ var port = parseInt(process.argv[2]) || 8080,
     filename = process.argv[3] || "hello-world.js";
 
 var server = http.createServer(function (req, res) {
+    // get file
     res.writeHead(200, { 'content-type': 'text/plain' });
     fs.createReadStream(filename).pipe(res);
 });
